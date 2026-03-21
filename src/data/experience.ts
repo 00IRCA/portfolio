0a1e239d788404ef951bcd7ff2c8f39d3e8671b8
Ismael Ruiz <ismaelruizcarlin4@gmail.com>
2026-03-21 12:46:52 +0100

    Add experience and technologies data

---
+++ src/data/experience.ts
@@ -0,0 +1,15 @@
+export const experience = [
+  {
+    position: 'Full Stack Developer',
+    company: 'INGELABS',
+    startYear: '2025',
+    description: 'Software Developer',
+  },
+  {
+    position: 'Frontend Developer',
+    company: 'INGELABS',
+    startYear: '2023',
+    endYear: '2025',
+    description: 'Software Developer',
+  },
+];
---
+++ src/data/technologies.ts
@@ -0,0 +1,13 @@
+export const technologies = [
+  { name: 'javascript', icon: '/technologies/javascript.png' },
+  { name: 'typescript', icon: '/technologies/typescript.svg' },
+  { name: 'react', icon: '/technologies/react.svg' },
+  { name: 'redux', icon: '/technologies/redux.svg' },
+  { name: 'node.js', icon: '/technologies/node.js.svg' },
+  { name: 'express.js', icon: '/technologies/express.js.svg' },
+  { name: 'bootstrap', icon: '/technologies/bootstrap.svg' },
+  { name: 'tailwind', icon: '/technologies/tailwind.svg' },
+  { name: 'sass', icon: '/technologies/sass.svg' },
+  { name: 'aws', icon: '/technologies/aws.svg' },
+  { name: 'git', icon: '/technologies/git.svg' },
+];
