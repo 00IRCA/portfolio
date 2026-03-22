39d1a11a3802d4acc68d55ce9031f25ddf3e8f69
Ismael Ruiz <ismaelruizcarlin4@gmail.com>
2026-03-22 20:51:20 +0100

    feat: add project constants for sections ids

---
+++ src/constants/sections.ts
@@ -0,0 +1,8 @@
+export const SECTION_IDS = {
+  home: 'home',
+  aboutMe: 'aboutMe',
+  technologies: 'technologies',
+  experience: 'experience',
+  projects: 'projects',
+  contact: 'contact',
+} as const;
