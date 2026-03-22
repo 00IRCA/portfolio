858d77290ab782062b4e8b31adf25fc0eaf67339
Ismael Ruiz <ismaelruizcarlin4@gmail.com>
2026-03-22 20:53:11 +0100

    feat: wire up page with all sections and update global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Footer from '../components/layout/Footer';
+import Header from '../components/layout/Header';
+import AboutMeSection from '../components/sections/AboutMeSection';
+import ContactSection from '../components/sections/ContactSection';
+import ExperienceSection from '../components/sections/ExperienceSection';
+import HeroSection from '../components/sections/HeroSection';
+import ProjectsSection from '../components/sections/ProjectsSection';
+import TechnologiesSection from '../components/sections/TechnologiesSection';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <AboutMeSection />
+      <TechnologiesSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <ContactSection />
+      <Footer />
+    </>
+  );
 }
